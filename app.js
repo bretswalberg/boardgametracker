a7984e84d5edf6f24f3efb7d34baf8b61bb8f351
Bret Swalberg <swalberb@oregonstate.edu>
2022-02-08 22:36:39 -0700

    Filled out rest of functionality

--- app.js
+++ app.js
@@ -16,66 +16,17 @@ app.set('port', portnum);
 
 
 // render homepage on initial GET request
-app.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
     res.render('home');
 });
-
 
 
 /*-------------------------
   GAME HANDLERS
 -------------------------*/
-app.get('/viewgame', function(req, res, next) {
-	var sqlPublisher = "SELECT g.*, p.name AS publisher " + 
-					" FROM game g LEFT JOIN publisher p ON g.publisherid = p.id " + 
-					" WHERE g.id = ?"
-	var sqlDesigner = "SELECT d.* " + 
-					" FROM designer d INNER JOIN gamedesigner gd ON d.id = gd.designerid " + 
-					" WHERE gd.gameid = ?"
-	var sqlMechanic = "SELECT m.* " + 
-					" FROM mechanic m INNER JOIN gamemechanic gm ON m.id = gm.mechanicid " + 
-					" WHERE gm.gameid = ?"
-	var sqlVideo = "SELECT v.* " + 
-					"FROM video v INNER JOIN gamevideo gv ON v.id = gv.videoid " + 
-					"WHERE gv.gameid = ?"
-	var sqlGamePhoto = "SELECT * FROM gamephoto WHERE gameid = ?"
-
-	db.all(sqlPublisher, [req.query.gid], (err, game) => {
-        if (err) {
-            next(err);
-            return;
-        }
-		db.all(sqlDesigner, [req.query.gid], (err, des) => {
-			if (err) {
-				next(err);
-				return;
-			}
-			db.all(sqlMechanic, [req.query.gid], (err, mech) => {
-				if (err) {
-					next(err);
-					return;
-				}
-				db.all(sqlVideo, [req.query.gid], (err, vid) => {
-					if (err) {
-						next(err);
-						return;
-					}
-					db.all(sqlGamePhoto, [req.query.gid], (err, photo) => {
-						if (err) {
-							next(err);
-							return;
-						}
-        				res.render('view-game', {game: game[0], designers: des, mechanics: mech, videos: vid, photos: photo});
-					});
-				});
-			});
-		});
-	});										
-});
-
 app.get('/viewgames', function(req, res, next) {
-	var sql = "SELECT g.*, p.name AS publisher " + 
-			" FROM game g LEFT JOIN publisher p ON g.publisherid = p.id " + 
+	var sql = "SELECT * " + 
+			" FROM game " + 
 			" ORDER BY title"
 	db.all(sql, [], (err, rows) => {
         if (err) {
@@ -86,26 +37,213 @@ app.get('/viewgames', function(req, res, next) {
     });
 });
 
-app.get('/newgame', function(req, res, next) {
-    var context = {};
-	var sqlDesigner = "SELECT id, firstname, lastname FROM designer ORDER BY lastname, firstname"
-	var sqlPublisher = "SELECT id, name FROM publisher ORDER BY name"
+app.get('/viewgame', function(req, res, next) {
+	var sqlGame = "SELECT *" + 
+					" FROM game g " + 
+					" WHERE id = ?"
+	var sqlVideo = "SELECT v.* " + 
+					"FROM video v INNER JOIN gamevideo gv ON v.id = gv.videoid " + 
+					"WHERE gv.gameid = ?"
+	var sqlGamePhoto = "SELECT * FROM gamephoto WHERE gameid = ?"
 
-	db.all(sqlDesigner, [], (err, drows) => {
+	db.all(sqlGame, [req.query.gid], (err, game) => {
         if (err) {
             next(err);
             return;
         }
-		db.all(sqlPublisher, [], (err, prows) => {
+		db.all(sqlVideo, [req.query.gid], (err, vid) => {
 			if (err) {
 				next(err);
 				return;
 			}
-    		res.render('add-game', {designers: drows, publishers: prows});
+			db.all(sqlGamePhoto, [req.query.gid], (err, photo) => {
+				if (err) {
+					next(err);
+					return;
+				}
+				res.render('view-game', {game: game[0], videos: vid, photos: photo});
+			});
+		});
+	});								
+});
+
+app.get('/newgame', function(req, res, next) {
+	res.render('add-game', {});
+});
+
+app.post('/newgame', function(req, res) {
+	var sql = "INSERT INTO game " + 
+			" (`title`, `year`, `publisher`, `minplayers`, `maxplayers`, " + 
+			" `minage`, `playtime`, `rating`, `bggid`) " + 
+			" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    params = [
+	req.body['game-title'],
+	req.body['game-year'] || null,
+	req.body['game-publisher'] || null,
+	req.body['game-min'] || null,
+	req.body['game-max'] || null,
+	req.body['game-age'] || null,
+	req.body['game-time'] || null,
+	req.body['game-rating'] || null,
+	req.body['game-bgg'] || null
+	]
+
+	db.run(sql, params, function(err, result) {
+		if (err) {
+			console.error(err.stack);
+            return;
+        }
+        return res.redirect('/viewgames');
+    });
+});
+
+app.get('/deletegame', function(req, res, next) {
+	var delPhoto = 'DELETE FROM gamephoto WHERE gameid = ?';
+	var delVideo = 'DELETE FROM gamevideo WHERE gameid = ?';
+	var delGame = 'DELETE FROM game WHERE id = ?';
+
+	db.run(delPhoto, [req.query.id], function(err, result) {
+		if (err) {
+			console.error(err.stack);
+			return;
+		}
+		db.run(delVideo, [req.query.id], function(err, result) {
+			if (err) {
+				console.error(err.stack);
+				return;
+			}
+			db.run(delGame, [req.query.id], function(err, result) {
+				if (err) {
+					console.error(err.stack);
+					return;
+				}
+				return res.redirect('/viewgames');
+			});
 		});
 	});
 });
 
+app.get('/updategame', function(req, res, next) {
+	sqlGame = 'SELECT * FROM game WHERE id = ?';
+	db.all(sqlGame, [req.query.id], function(err, game) {
+		if (err) {
+			next(err);
+			return;
+		}
+		res.render('update-game', {data: game[0]});
+	});
+});
+
+app.post('/updategame', function(req, res, next) {
+	var sql = 'UPDATE game SET title=?, year=?, publisher=?, minplayers=?, ' + 
+			' maxplayers=?, minage=?, playtime=?, rating=?, bggid=? ' + 
+			' WHERE id = ?';
+	var params = [req.body['game-title'],
+				req.body['game-year'] || null,
+				req.body['game-publisher'] || null,
+				req.body['game-min'] || null,
+				req.body['game-max'] || null,
+				req.body['game-age'] || null,
+				req.body['game-time'] || null,
+				req.body['game-rating'] || null,
+				req.body['game-bgg'] || null,
+				req.body['game-id'] ];
+
+    db.run(sql, params, function(err, result) {
+		if (err) {
+			next(err);
+			return;
+		}
+		return res.redirect('/viewgames');
+   	});
+});
+
+/*-------------------------
+  PHOTO HANDLERS
+-------------------------*/
+app.get('/viewphotos', function(req, res, next) {
+	sql = 'SELECT gp.id, gp.image, gp.description, gp.gameid, g.title ' + 
+		' FROM gamephoto gp INNER JOIN game g on gp.gameid = g.id ' +
+		' ORDER BY g.title, gp.description';
+    db.all(sql, [], function(err, rows) {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('view-photos', {data: rows});
+    });
+});
+
+app.get('/newphoto', function(req, res, next) {
+    db.all('SELECT id, title FROM game ORDER BY title', function(err, gamerows) {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('add-photo', {data: gamerows});
+    });
+});
+
+app.post('/newphoto', function(req, res) {
+	sql = 'INSERT INTO gamephoto (gameid, image, description) VALUES(?, ?, ?)';
+	params = [req.body['photo-gameid'], req.body['photo-url'], req.body['photo-desc']];
+	db.run(sql, params, function (err, result) {
+		if (err) {
+			console.error(err.stack);
+			next(err);
+			return;
+		}
+		return res.redirect('/viewphotos');
+	});
+});
+
+app.get('/deletephoto', function(req, res, next) {
+	db.run('DELETE FROM gamephoto WHERE id = ?', [req.query.id], function(err, rows) {
+		if (err) {
+			next(err);
+			return;
+		}
+		return res.redirect('/viewphotos');
+	});
+});
+
+
+/*----------------
+OTHER HANDLERS
+-----------------*/
+
+// render homepage on initial GET request
+app.get('/search', function(req, res, next) {
+	const sql = "SELECT id, title, publisher " +
+					"FROM game " +
+					"WHERE (title LIKE ? " +
+					"  OR publisher LIKE ? ) ";
+	var params = ['%' + req.query.s + '%', '%' + req.query.s + '%']; 
+	db.all(sql,	params, (err, rows) => {
+		if (err) {
+			next(err);
+			return;
+		}
+		res.render('search', {data: rows});
+	});
+});
+
+app.post('/newvideo', function(req, res) {
+	mysql.pool.query("INSERT INTO video (`title`, `url`, `type`) VALUES(?, ?, ?)",
+	[
+	req.body['vid-title'],
+	req.body['vid-url'],
+	req.body['vid-type']
+	],
+	function (err, result) {
+		if (err) {
+			console.error(err.stack);
+			next(err);
+			return;
+		}
+		res.render('view-video');
+	});
+});
 
 app.use(function(req,res){
   res.status(404);
